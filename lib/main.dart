30b5023c08845a5273edf95091e8a4a4d35c4f2f
Varad-01 <varadsankhe01@gmail.com>
2023-03-01 18:39:31 +0530

    Update Feature added

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
 	Widget build(BuildContext context) {
 	return MaterialApp(
 		title: 'Flutter',
-		initialRoute: '/homePage3',
+		initialRoute: '/welcomePage',
 		routes: {
 			'/splashScreen':(context)=>SplashScreen(),
 			'/welcomePage':(context)=>WelcomePage(),
